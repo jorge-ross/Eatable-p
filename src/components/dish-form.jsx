3a8562154fb82c92a6ae9c3d89c37e3e3958f7b3
Jorge Ross <jorg.ross.13@gmail.com>
2023-08-01 22:38:39 -0600

    add editable form to dish edit

--- src/components/dish-form.jsx
+++ src/components/dish-form.jsx
@@ -19,85 +19,166 @@ const Container = styled.div`
   flex-direction: column;
   padding-bottom: 16px;
   gap: 5px;
-  
 `;
 
-const Data = styled.p`
+const Data = styled.input`
   ${typography.text.xx}
   margin: 0;
   font-weight: 400;
   padding-bottom: 5px;
+  border: none;
   border-bottom: 1px solid black;
 `;
 
-const FieldDescription = styled.p`
+const FieldDescription = styled.input`
   ${typography.text.xx}
   height: 146px;
+  width: 350px;
   margin: 0;
   font-weight: 400;
   padding-bottom: 5px;
+  border: none;
   border-bottom: 1px solid black;
 `
 
-const FieldURL = styled.div`
+const FieldURL = styled.input`
   ${typography.text.xx}
-  height: 55px;
+  height: 65px;
+  width: 100%
   margin: 0;
   font-weight: 400;
   padding-bottom: 5px;
+  border: none;
   border-bottom: 1px solid black;
+  overflow: hidden;
+  white-space: pre-wrap;
 `
 
 function DishForm() {
   const {id} = useParams();
-  console.log(id);
-  
+  // console.log(id);
+
   const [product, setProduct] = useState({})
 
   useEffect(() => {
-    showDish(id).then(setProduct).catch(console.log)
+    showDish(id).then(setProduct).catch()
+  }, [id]);
+  
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [description, setDescription] = useState("");
+  const [category, setCategory] = useState("");
+  const [picture_url, setPicture_url] = useState("");
+
+  useEffect(() => {
+    showDish(id).then(product => {
+      setName(product.name);
+      setPrice(product.price);
+      setDescription(product.description);
+      setCategory(product.category);
+      setPicture_url(product.picture_url);
+    }).catch(console.log)
   }, [id]);
 
-  console.log(product);
+  const handleChange = (event) => {
+    const {name, value} = event.target;
+    switch (name) {
+      case "name":
+        setName(value);
+        break;
+      case "price":
+        setPrice(value);
+        break;
+      case "description":
+        setDescription(value);
+        break;
+      case "category":
+        setCategory(value);
+        break;
+      case "picture_url":
+        setPicture_url(value);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newData = {
+      name,
+      price,
+      description,
+      category,
+      picture_url
+    };
+
+    editDish(id, newData).then(response => {
+      console.log(response);
+    }).catch(console.log)
+  };
 
   
     return (
         <Body>
+          <form onSubmit={handleSubmit}>
           <Container>
             <label htmlFor="name">Name</label>
-            <Data> 
-              {product?.name}
-            </Data>
+            <Data
+            type="text" 
+            id="name" 
+            name="name" 
+            value={name} 
+            onChange={handleChange}
+            />
           </Container>
           <Container>
             <label htmlFor="name">Price</label>
-            <Data> 
-              {product?.price}
-            </Data>
+            <Data
+            type="integer" 
+            id="price" 
+            name="price" 
+            value={price} 
+            onChange={handleChange}
+            />
           </Container>
           <Container>
             <label htmlFor="name">Description (Max 150 characters)</label>
-            <FieldDescription> 
-              {product?.description}
-            </FieldDescription>
+            <FieldDescription
+              type="text"
+              id="description"
+              name="description"
+              value={description}
+              onChange={handleChange}
+            /> 
+              
           </Container>
           <Container>
             <label htmlFor="name">Category</label>
-            <Data> 
-              {product?.category}
-            </Data>
+            <Data 
+              type="text"
+              id="category"
+              name="category"
+              value={category}
+              onChange={handleChange}
+            />
           </Container>
           <Container>
             <label htmlFor="name">Picture URL</label>
-            <FieldURL> 
-              {product?.picture_url}
-            </FieldURL>
+            <FieldURL
+            type="text"
+            id="picture_url"
+            name="picture_url"
+            value={picture_url}
+            onChange={handleChange}              
+            />
           </Container>
-          <Link to={`/products/${product.id}`}>
+          <Link to={`/products/${product?.id}`}>
             <Button type="submit">
             Save
             </Button>
           </Link>
+          </form>
         </Body>
     );
 }
